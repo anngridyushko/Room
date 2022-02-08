07359dc92e5c57134890499ed4fd87608d83d7b7
anngridyushko <annagridyushko@yandex.by>
2022-02-09 00:30:03 +0300

    Done

--- app/src/main/java/com/example/roomcodelab/data/db/WordDAO.kt
+++ app/src/main/java/com/example/roomcodelab/data/db/WordDAO.kt
@@ -1,10 +1,12 @@
 package com.example.roomcodelab.data.db
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface WordDAO {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
--- app/src/main/java/com/example/roomcodelab/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/roomcodelab/ui/activities/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.roomcodelab.ui.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomcodelab.R
+import com.example.roomcodelab.data.db.Word
+import com.example.roomcodelab.ui.WordApplication
 import com.example.roomcodelab.ui.recycler.WordListAdapter
+import com.example.roomcodelab.ui.viewmodel.WordViewModel
+import com.example.roomcodelab.ui.viewmodel.WordViewModelFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newWordActivityRequestCode = 1
+
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
+        wordViewModel.words.observe(this,
+            Observer { words ->
+            words?.let { adapter.submitList(it) }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode &&
+            resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(AddWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
